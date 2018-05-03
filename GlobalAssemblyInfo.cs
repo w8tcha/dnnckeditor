7562d15d5be2b0e836bb90d11ad2c0ac60e9def8
w8tcha <thewatcher@watchersnet.de>
2018-05-03 10:47:27 +0200

    bump to 02.01.16

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("2.01.15")]
-[assembly: AssemblyFileVersion("2.01.1.5")]
+[assembly: AssemblyVersion("2.01.16")]
+[assembly: AssemblyFileVersion("2.01.1.6")]
